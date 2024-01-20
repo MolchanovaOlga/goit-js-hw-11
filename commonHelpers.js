50f68eb92c5899eff5edb69773ec9b3ffde4d81b
MolchanovaOlga <92665850+MolchanovaOlga@users.noreply.github.com>
2024-01-20 21:12:22 +0000

    Deploying to gh-pages from @ MolchanovaOlga/goit-js-hw-11@3988aaffbc46de7ab58639c6e75dedbbb7fd0068 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{i as h,S as d}from"./assets/vendor-46aac873.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&r(a)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();const g="/goit-js-hw-11/assets/bi_x-octagon-aed43fc7.svg",n=document.querySelector("form"),p=document.querySelector("textarea"),c=document.querySelector(".gallery"),l=document.querySelector("span");n.addEventListener("submit",o=>{o.preventDefault(),c.textContent="";const s=p.value;y(s),n.reset()});function y(o){l.classList.add("loader");const i=`https://pixabay.com/api/?${new URLSearchParams({key:"41764698-0ccaaf72f9cf319226b6a04c5",q:o,image_type:"photo",orientation:"horizontal",safesearch:!0})}`;fetch(i).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()}).then(r=>{l.classList.remove("loader");let e=r.hits;if(e.length==0){L();return}b(e)}).catch(r=>console.log(r))}function L(){h.error({message:"Sorry, there are no images matching<br/>your search query. Please try again!",position:"topRight",backgroundColor:"#EF4040",messageColor:"#fff",iconUrl:g})}function b(o){const s=o.map(({webformatURL:r,largeImageURL:e,tags:t,likes:a,views:u,comments:f,downloads:m})=>`
+import{i as l,S as d}from"./assets/vendor-46aac873.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&r(a)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();const c="/goit-js-hw-11/assets/bi_x-octagon-aed43fc7.svg",n=document.querySelector("form"),y=document.querySelector("textarea"),u=document.querySelector(".gallery"),f=document.querySelector("span");n.addEventListener("submit",s=>{s.preventDefault(),f.classList.add("loader"),u.textContent="";const o=y.value;p(o),n.reset()});function p(s){const i=`https://pixabay.com/api/?${new URLSearchParams({key:"41764698-0ccaaf72f9cf319226b6a04c5",q:s,image_type:"photo",orientation:"horizontal",safesearch:!0})}`;fetch(i).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()}).then(r=>{let e=r.hits;if(e.length==0){L();return}w(e)}).catch(r=>{console.log(r),b()}).finally(()=>f.classList.remove("loader"))}function L(){l.error({message:"Sorry, there are no images matching<br/>your search query. Please try again!",position:"topRight",backgroundColor:"#EF4040",messageColor:"#fff",iconUrl:c})}function b(){l.error({message:"Sorry, an error occurred. Please try again later!",position:"topRight",backgroundColor:"#EF4040",messageColor:"#fff",iconUrl:c})}function w(s){const o=s.map(({webformatURL:r,largeImageURL:e,tags:t,likes:a,views:m,comments:g,downloads:h})=>`
         <li class="gallery-item">
             <a class="gallery-link" href="${e}">
             <img
@@ -14,17 +14,17 @@ import{i as h,S as d}from"./assets/vendor-46aac873.js";(function(){const s=docum
                 </li>
                 <li class="statistics-item">
                     <h2>Views</h2>
-                    <p>${u}</p>
+                    <p>${m}</p>
                 </li>
                 <li class="statistics-item">
                     <h2>Comments</h2>
-                    <p>${f}</p>
+                    <p>${g}</p>
                 </li>
                 <li class="statistics-item">
                     <h2>Downloads</h2>
-                    <p>${m}</p>
+                    <p>${h}</p>
                 </li>
             </ul>
         </li>
-        `).join("");c.insertAdjacentHTML("beforeend",s),new d(".gallery a",{captionsData:"alt",captionDelay:250}).refresh()}
+        `).join("");u.insertAdjacentHTML("beforeend",o),new d(".gallery a",{captionsData:"alt",captionDelay:250}).refresh()}
 //# sourceMappingURL=commonHelpers.js.map
